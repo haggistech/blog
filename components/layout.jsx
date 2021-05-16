8339ce02daefb6ceec6544d0d447910b88063a08
haggistech <haggistech@gmail.com>
2021-05-16 16:32:51 +0100

    adding more padding around post text

--- components/layout.jsx
+++ components/layout.jsx
@@ -175,7 +175,6 @@ export default function Layout({ children }) {
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
